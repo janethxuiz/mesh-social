519b3aa6f5af12dcf4b78ab275f906933cdb9eca
janethxuiz <janethruiz152@gmail.com>
2022-09-10 02:23:50 -0700

    created add reaction and to be able to remove a reaction

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -92,5 +92,31 @@ const ThoughtController = {
         .catch((err) => res.json(err));
     },
 
-    
-}
+    addReaction({ params, body }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId },
+            { $addToSet: { reactions: body } },
+            { new: true, runValidators: true }
+        )
+        .then((dbThoughtData) => {
+            if (!dbThoughtData) {
+                res.status(404).json({ message: "No thought with this id" });
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch((err) => res.json(err));
+    },
+
+    addReaction({ params, body }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId },
+            { $pull: { reactions: { reactionId: params.reactionId } } },
+            { new: true }
+        )
+        .then((dbThoughtData) => res.json(dbThoughtData))
+        .catch((err) => res.json(err));
+    },
+};
+
+module.exports = ThoughtController;
