d3ac70c405bd7ba21110418f439b93f7f46debca
janethxuiz <janethruiz152@gmail.com>
2022-09-09 18:09:03 -0700

    thought js files connected and coded

---
+++ models/thought.js
@@ -0,0 +1,74 @@
+const { createPromptModule } = require("inquirer");
+const { Schema, model, Types } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280,
+        },
+
+        username: {
+            type: String,
+            required: true,
+        },
+
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (timestamp) => dateFormat(timestamp),
+        },
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+const ThoughSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: "Thought is Required",
+            minlength: 1,
+            maxlength: 280,
+        },
+
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (timestamp) => dateFormat(timestamp),
+        },
+
+        username: {
+            type: String
+            required: true,
+        },
+
+        reactions: [ReactionSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+ThoughSchema.virtual("reactionCount").get(function () {
+    return this.reactions.length;
+});
+
+const Thought = model("Thought", ThoughSchema);
+
+module.exports = Thought;
