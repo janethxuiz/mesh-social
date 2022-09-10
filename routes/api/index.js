2b8ff20a97c8e0ac68ff8ef4839f8af3da84b99a
janethxuiz <janethruiz152@gmail.com>
2022-09-10 01:11:08 -0700

    thought routes file added

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const userRoutes = require("./userRoutes");
-const thoughRoutes = require("./thoughRoutes");
+const thoughRoutes = require("./thoughtRoutes");
 
 router.use("/users", userRoutes);
 router.use ("/thoughts", thoughRoutes);
