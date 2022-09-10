c5164008f7db940588cb105d4ee2ad766d8567f7
janethxuiz <janethruiz152@gmail.com>
2022-09-10 00:51:01 -0700

    updated index in api file

---
+++ routes/api/index.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+const userRoutes = require("./userRoutes");
+const thoughRoutes = require("./thoughRoutes");
+
+router.use("/users", userRoutes);
+router.use ("/thoughts", thoughRoutes);
+
+module.exports = router;
