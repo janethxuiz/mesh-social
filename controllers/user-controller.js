583b4104aea29dc540ecc427c88b026bcb871b16
janethxuiz <janethruiz152@gmail.com>
2022-09-10 19:13:58 -0700

    created ability to add friends and remove friends

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -76,5 +76,36 @@ const userController = {
         .catch((err) => res.json(err));
     },
 
-    
-}
+    addFriend({ params }, res) {
+        User.findOneAndUpdate(
+            { _id: params.userId },
+            { $addToSet: { friends: params.friendId } },
+            { new: true, runValidators: true }
+        )
+        .then((dbUserData) => {
+            if (!dbUserData) {
+                res.status(404).json({ message: "no user with this id"});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch((err) => res.json(err));
+    },
+
+    removeFriend({ params }, res) {
+        User.findOneAndDelete(
+            { _id: params.userId },
+            { $pull: { friends: params.friendId } },
+            { new: true }
+        )
+        .then((dbUserData) => {
+            if (!dbUserData) {
+                return res.status(404).json({ message: "no user with this id"});
+            }
+            res.json(dbUserData);
+        })
+        .catch((err) => res.json(err));
+    },
+};
+
+module.exports = userController;
