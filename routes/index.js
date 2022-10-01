fab2eec93cfcfd686a373304377ee4b94e919bdd
janethxuiz <janethruiz152@gmail.com>
2022-09-30 18:15:40 -0700

    removing code

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,5 @@ const router = require("express").Router();
 const apiRoutes = require("./api");
 
 router.use("/api", apiRoutes);
-router.use((req, res) => {
-    return res.send("404 error");
-});
 
 module.exports = router;
